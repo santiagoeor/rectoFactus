2c2a5bc31fdfe27aa05b7ca6b271544d2f595566
santiago <everardoorozcocarmona@gmail.com>
2025-09-14 18:40:23 -0500

    add enviroments

---
+++ src/env/enviroment.ts
@@ -0,0 +1,9 @@
+
+const env = {
+    url_api: "https://api-sandbox.factus.com.co",
+    grant_type: "password",
+    client_id: "9df0313d-5a87-4da5-b4f0-04ff56e3b43e",
+    client_secret: "adx6NWWHGJ2seMPGs2zTSsLjuhQHfn6s7BeCjd87",
+};
+
+export const enviroment = env;
