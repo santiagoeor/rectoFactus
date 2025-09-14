b849b537517b20cec1c8e2794682c30f8c2210a0
Santiago Orozco <93175401+santiagoeor@users.noreply.github.com>
2025-09-14 18:52:40 -0500

    deletefiles

--- src/env/enviroment.ts
+++
@@ -1,9 +0,0 @@
-
-const env = {
-    url_api: "https://api-sandbox.factus.com.co",
-    grant_type: "password",
-    client_id: "9df0313d-5a87-4da5-b4f0-04ff56e3b43e",
-    client_secret: "adx6NWWHGJ2seMPGs2zTSsLjuhQHfn6s7BeCjd87",
-};
-
-export const enviroment = env;
